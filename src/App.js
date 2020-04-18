a78497834078e1df5fced7fdea79130a0a358698
Mwai Joseph <=>
2020-04-18 05:49:44 +0300

    Upadate menu-item to navigate withRouterHOC

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
@@ -12,8 +12,10 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/' component={HatsPage}/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route  path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
