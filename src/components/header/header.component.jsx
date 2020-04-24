7c7fc2641821a3ef564d39fcd41128ae21e16b36
Mwai Joseph <=>
2020-04-24 16:54:34 +0300

    added cart icon and cart dropdown components to our app

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,9 +5,12 @@ import { auth } from '../../firebase/firebase.utils';
 
 import { connect } from 'react-redux';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link className="logo-container" to='/'>
             <Logo className="logo"/>
@@ -33,13 +36,23 @@ const Header = ({ currentUser }) => (
                     </Link>
                 )
             }
+
+            <CartIcon />
         </div>
+
+        {/* Cart dropdown sits just below options div */}
+        {/* dynamically render cart dropdown based on hidden value */}
+
+        { hidden ? null : <CartDropdown />}
+
+
     </div>
 );
 
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: {currentUser}, cart: {hidden} }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
