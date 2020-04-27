ad9d2b9d850002a406a42f5bf8d0aaf480008d4e
Mwai Joseph <=>
2020-04-27 18:19:04 +0300

    added addItem and removeItem functionality to cartItem

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,12 +1,14 @@
+import CartItem from "../../components/cart-item/cart-item.component";
+
 export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     const existingCartItem = cartItems.find(
-        cartItem => cartItem.id == cartItemToAdd.id
+        cartItem => cartItem.id === cartItemToAdd.id
     );
 
     if(existingCartItem) {
         return cartItems.map(cartItem => 
-            cartItem.id == cartItemToAdd.id
+            cartItem.id === cartItemToAdd.id
             ? { ...cartItem, quantity: cartItem.quantity + 1 }
             : cartItem
             )
@@ -15,3 +17,22 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     return [ ...cartItems, { ...cartItemToAdd, quantity: 1 }];
 
 } 
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    return cartItems.map(
+        cartItem => cartItem.id === cartItemToRemove.id 
+          ? 
+            { ...cartItem, quantity: cartItem.quantity-1 }
+              :
+            cartItem
+    )
+
+}
